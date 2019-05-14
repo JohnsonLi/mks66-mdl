cf501a87818dbcd6e4e55fa0235ff701afed8fd9
Johnson Li <jli39@stuy.edu>
2019-05-13 21:56:00 -0700

    done i guess

--- script.py
+++ script.py
@@ -32,6 +32,8 @@ def run(filename):
     tmp = new_matrix()
     ident( tmp )
 
+    edges = []
+    polygons = []
     stack = [ [x[:] for x in tmp] ]
     screen = new_screen()
     zbuffer = new_zbuffer()
@@ -65,13 +67,42 @@ def run(filename):
                 else:
                     t = make_rotZ(theta)
                 matrix_mult( stack[-1], t )
-                systems[-1] = [ x[:] for x in t]
+                stack[-1] = [ x[:] for x in t]
             elif op == 'scale':
                 t = make_scale(float(args[0]), float(args[1]), float(args[2]))
                 matrix_mult( stack[-1], t )
                 stack[-1] = [ x[:] for x in t]
-            elif op == 'box':
-                pass
+            elif op == 'box' or op == 'sphere' or op == 'torus':
+                if op == 'box':
+                    add_box(polygons,
+                        float(args[0]), float(args[1]), float(args[2]),
+                        float(args[3]), float(args[4]), float(args[5]))
+                if op == 'sphere':
+                    add_sphere(polygons,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+                if op == 'torus':
+                    add_torus(polygons,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), step_3d)
+
+                matrix_mult( stack[-1], polygons )
+
+                if(command['constants']):
+                    draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+                else:
+                    draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+               
+                polygons = []
+            elif op == 'line':
+                add_edge( edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+                matrix_mult( systems[-1], edges )
+                draw_lines(eges, screen, zbuffer, color)
+                edges = []
+            elif command['op'] == 'save':
+                save_extension(screen, args[0] + ".png")
 
         else:
             if op == 'push':
@@ -80,5 +111,3 @@ def run(filename):
                 stack.pop()
             elif op == 'display':
                 display(screen)
-
-        # print command['op']
